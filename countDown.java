3c2871df615ee2452cbb7ccb9a72d9402a441178
adarvin14 <amdarvin14@gmail.com>
2021-10-13 15:02:47 -0400

    countdown complete

--- countDown.java
+++ countDown.java
@@ -1,7 +1,7 @@
 class countDown {
 	public static void main(String[] args) {
-		for (int i = 0; i =< 5; i++) {
- 			 System.out.println(i);
+		for (int i = 10; i > 0; i--) {
+ 			 System.out.println("t minus " + i );
 		}	
 	}
 }
