bab122b325680da171562f17f11fb569a5269cc1
adarvin14 <amdarvin14@gmail.com>
2021-10-14 18:01:11 -0400

    models/classes practice

---
+++ com/revature/driver/Driver.java
@@ -0,0 +1,18 @@
+import com.revature.models.HumAthletean;
+
+public class Driver{
+//add attributes / parameters for the objects that will be created
+
+    public String name;
+    public String sport;
+    public int age;
+    public boolean active;
+
+    public Athlete(){
+
+    }
+
+    public String championships(String title){
+        return ""
+    }
+}
---
+++ com/revature/models/Athlete.java
@@ -0,0 +1,7 @@
+package com.revature.models;
+
+public class Athlete{
+    public static void main(String[] args){
+
+    }
+}
