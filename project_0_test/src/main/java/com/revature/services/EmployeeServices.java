b7cf4e61049503210e5de1b474b8936839328615
adarvin14 <amdarvin14@gmail.com>
2021-11-09 21:43:53 -0500

    customer and employee services done

---
+++ project_0_test/src/main/java/com/revature/services/EmployeeServices.java
@@ -0,0 +1,44 @@
+package com.revature.services;
+
+import java.util.List;
+
+import com.revature.repositories.EmployeeDao;
+// import com.revature.repositories.EmployeeList;
+import com.revature.repositories.EmployeePostgres;
+import com.revature.exceptions.LoginException;
+import com.revature.exceptions.EmailTakenException;
+import com.revature.models.Employee;
+
+public class EmployeeServices {
+	private static EmployeeDao ed = new EmployeePostgres();
+	
+	public Employee addEmployee(Employee e) throws EmailTakenException {
+		Employee newEmployee = this.getEmployeeByEmail(e.getEmail());
+		if(newEmployee != null) {
+			throw new EmailTakenException();
+		}
+		
+		return ed.add(e);
+	}
+	
+	public Employee getEmployeeByEmail(String email) {
+		List<Employee> employees = ed.getAll();
+		for(Employee e : employees) {
+			if(e.getEmail().equals(email)) {
+				return e;
+			}
+		}
+		return null;
+	}
+	
+	public Employee login(String email, String password) throws LoginException {
+		Employee emp = this.getEmployeeByEmail(email);
+		if(emp == null || !emp.getPassword().equals(password)) {
+			throw new LoginException();
+		}
+		if(emp.getEmployeeID() == 987) {
+			
+		}
+		return emp;
+	}
+}
